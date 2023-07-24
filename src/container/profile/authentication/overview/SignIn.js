587370194644253351c34610ed8e9c4d48e58a4e
Casko12 <phamduyhung.ftu@gmail.com>
2023-07-24 23:03:14 +0700

    Upcode

--- src/container/profile/authentication/overview/SignIn.js
+++ src/container/profile/authentication/overview/SignIn.js
@@ -1,6 +1,3 @@
-import UilFacebook from '@iconscout/react-unicons/icons/uil-facebook-f';
-import UilGithub from '@iconscout/react-unicons/icons/uil-github';
-import UilTwitter from '@iconscout/react-unicons/icons/uil-twitter';
 import { Button, Col, Form, Input, Row } from 'antd';
 import { Auth0Lock } from 'auth0-lock';
 import React, { useCallback, useState } from 'react';
@@ -8,7 +5,7 @@ import React, { useCallback, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { ReactSVG } from 'react-svg';
+
 import { AuthFormWrap } from './style';
 import { Checkbox } from '../../../../components/checkbox/checkbox';
 import { auth0options } from '../../../../config/auth0';
@@ -55,7 +52,7 @@ function SignIn() {
       <Col xxl={6} xl={8} md={12} sm={18} xs={24}>
         <AuthFormWrap>
           <div className="ninjadash-authentication-top">
-            <h2 className="ninjadash-authentication-top__title">Đăng nhập</h2>
+            <h2 className="ninjadash-authentication-top__title">Sign In</h2>
           </div>
           <div className="ninjadash-authentication-content">
             <Form name="login" form={form} onFinish={handleSubmit} layout="vertical">
@@ -83,42 +80,11 @@ function SignIn() {
                   {isLoading ? 'Loading...' : 'Sign In'}
                 </Button>
               </Form.Item>
-              <p className="ninjadash-form-divider">
-                <span>Or</span>
-              </p>
-              <ul className="ninjadash-social-login">
-                <li>
-                  <Link className="google-social" to="#">
-                    <ReactSVG src={require(`../../../../static/img/icon/google-plus.svg`).default} />
-                  </Link>
-                </li>
-                <li>
-                  <Link className="facebook-social" to="#">
-                    <UilFacebook />
-                  </Link>
-                </li>
-                <li>
-                  <Link className="twitter-social" to="#">
-                    <UilTwitter />
-                  </Link>
-                </li>
-                <li>
-                  <Link className="github-social" to="#">
-                    <UilGithub />
-                  </Link>
-                </li>
-              </ul>
-              <div className="auth0-login">
-                <Link to="#" onClick={() => lock.show()}>
-                  SignIn with Auth0
-                </Link>
-                <Link to="/fbSignIn">SignIn With Firebase</Link>
-              </div>
             </Form>
           </div>
           <div className="ninjadash-authentication-bottom">
             <p>
-              Don`t have an account?<Link to="/register">Sign up</Link>
+              Cannot login?<Link to="/register">Contact system admin</Link>
             </p>
           </div>
         </AuthFormWrap>
